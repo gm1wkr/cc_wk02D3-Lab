ea480440ce459f1bddc8574eeb1b6f63f5b6104e
GM1WKR <gm1wkr@gmail.com>
2022-05-25 17:12:13 +0100

    decrease_wallet refactored (check enough money)

--- src/customer.py
+++ src/customer.py
@@ -6,7 +6,8 @@ class Customer:
         self.drunk_level = 0
 
     def decrease_wallet(self, amount):
-        self.wallet -= amount
+        if self.wallet >= amount:
+            self.wallet -= amount
 
     def increase_drunk_level(self, unit):
         self.drunk_level += unit
